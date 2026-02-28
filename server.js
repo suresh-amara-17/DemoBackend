19a0600cf3845768ce434d715ef6674082bd36ef
venugopl <venugopal.batthina@gmail.com>
2026-02-28 21:40:26 +0530

    Add CI/CD Code Updated

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 sequelize.sync({ alter: true }).then(() => {
-  app.listen(PORT, () => console.log(`Server ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch(err => {
   console.error('DB connection failed', err);
 });
